cc6547089fb929da203d9295af9df40ab96548a4
Alvin Febriando <alvin.febriando@shopee.com>
2023-12-02 11:49:58 +0700

    feat: add string hasher

---
+++ hash/hasher.go
@@ -0,0 +1,25 @@
+package hash
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Hasher interface {
+	Hash(text string) ([]byte, error)
+	Compare(hashed string, text string) bool
+}
+
+type bcryptHasher struct{}
+
+func NewHasher() Hasher {
+	return &bcryptHasher{}
+}
+
+func (h *bcryptHasher) Hash(text string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(text), 10)
+}
+
+func (h *bcryptHasher) Compare(hashed string, text string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(text))
+	return err == nil
+}
