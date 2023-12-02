a29c6f07b60df5584617906e89cf849f246b863e
Alvin Febriando <alvin.febriando@shopee.com>
2023-12-02 12:30:11 +0700

    feat: add query and condition

---
+++ valueobject/condition.go
@@ -0,0 +1,41 @@
+package valueobject
+
+import "fmt"
+
+type Operator string
+
+const (
+	Equal            Operator = "="
+	NotEqual                  = "!="
+	LessThan                  = "<"
+	GreaterThan               = ">"
+	LessThanEqual             = "<="
+	GreaterThanEqual          = ">="
+	Is                        = "IS"
+	In                        = "IN"
+	Not                       = "NOT"
+	Like                      = "LIKE"
+	NotLike                   = "NOT LIKE"
+	ILike                     = "ILIKE"
+	NotILike                  = "NOT ILIKE"
+)
+
+type Condition struct {
+	Field     string
+	Operation Operator
+	Value     any
+}
+
+func NewCondition(field string, operation Operator, value any) *Condition {
+	if value == "" {
+		return nil
+	}
+	if operation == ILike {
+		value = "%" + fmt.Sprintf("%v", value) + "%"
+	}
+	return &Condition{
+		Field:     field,
+		Operation: operation,
+		Value:     value,
+	}
+}
---
+++ valueobject/query.go
@@ -0,0 +1,47 @@
+package valueobject
+
+type Query struct {
+	Page       int
+	PerPage    int
+	OrderedBy  string
+	Conditions []*Condition
+	With       []string
+	Locked     bool
+}
+
+func NewQuery() *Query {
+	query := &Query{}
+	query.Conditions = make([]*Condition, 0)
+	return query
+}
+
+func (q *Query) Condition(field string, operator Operator, value any) *Query {
+	condition := NewCondition(field, operator, value)
+	q.Conditions = append(q.Conditions, condition)
+	return q
+}
+
+func (q *Query) IsConditionExist(field string) bool {
+	for _, condition := range q.Conditions {
+		if condition.Field == field {
+			return true
+		}
+	}
+	return false
+}
+
+func (q *Query) Paginate(page int, perPage int) *Query {
+	q.Page = page
+	q.PerPage = perPage
+	return q
+}
+
+func (q *Query) Order(orderedBy string) *Query {
+	q.OrderedBy = orderedBy
+	return q
+}
+
+func (q *Query) Lock() *Query {
+	q.Locked = true
+	return q
+}
