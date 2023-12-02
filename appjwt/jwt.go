b3170ea3579c106048a9366b5c78b7721a7083ed
Alvin Febriando <alvin.febriando@shopee.com>
2023-12-02 13:12:36 +0700

    feat: implement jwt

---
+++ appjwt/jwt.go
@@ -0,0 +1,67 @@
+package appjwt
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/alvinfebriando/gin-gorm-skeleton/entity"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type CustomClaims struct {
+	jwt.RegisteredClaims
+	Id    uint
+	Email string
+}
+
+type Jwt interface {
+	GenerateToken(user *entity.User) (string, error)
+	ValidateToken(tokenString string) (*entity.User, error)
+}
+
+type jwtImpl struct {
+	secretKey []byte
+}
+
+func NewJwt() Jwt {
+	key := os.Getenv("SECRET")
+	return &jwtImpl{
+		secretKey: []byte(key),
+	}
+}
+
+func (j *jwtImpl) GenerateToken(user *entity.User) (string, error) {
+	userId := strconv.Itoa(int(user.Id))
+	claims := CustomClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "LIBRARY",
+			Subject:   userId,
+		},
+		Id: user.Id,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, err := token.SignedString(j.secretKey)
+	return signedString, err
+}
+
+func (j *jwtImpl) ValidateToken(tokenString string) (*entity.User, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.secretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*CustomClaims); ok {
+		user := &entity.User{
+			Id:    claims.Id,
+			Email: claims.Email,
+		}
+		return user, nil
+	}
+	return nil, errors.New("invalid claims type")
+}
