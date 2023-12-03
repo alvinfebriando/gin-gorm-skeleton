976c38d9d4e3934ad72e0bd313afebf372e0af19
Alvin Febriando <alvin.febriando@shopee.com>
2023-12-03 09:15:57 +0700

    feat: add timestamp value to entity

--- entity/user.go
+++ entity/user.go
@@ -1,8 +1,17 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Id       uint   `gorm:"primaryKey;autoIncrement"`
-	Name     string `gorm:"not null"`
-	Email    string `gorm:"not null"`
-	Password string `gorm:"not null"`
+	Id        uint   `gorm:"primaryKey;autoIncrement"`
+	Name      string `gorm:"not null"`
+	Email     string `gorm:"not null"`
+	Password  string `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
 }
